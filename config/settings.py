60bc902c31ac0c755290e6f957ae691df7d1fbad
denisbelenko <denisdel123@>
2025-02-20 20:54:53 +0700

    изменил ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = constants.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = constants.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
