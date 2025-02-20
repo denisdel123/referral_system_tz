c5081ce6cddbf1fd206f9730974278bcb2054fe0
denisbelenko <denisdel123@>
2025-02-20 16:36:06 +0700

    создал эндпоинт для создания реферального кода

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework import generics
+from rest_framework import generics, permissions
 from . import serializers as users_serializers
 from . import models as users_models
 
@@ -11,3 +11,17 @@ from . import models as users_models
 class RegistrationUsers(generics.CreateAPIView):
     serializer_class = users_serializers.RegistrationUsersSerializer
     queryset = users_models.User.objects.all()
+
+
+
+@extend_schema(
+    summary="Создание реферального кода.",
+    description="Создает реферальный код и добавляет его к пользователю.",
+)
+class CreateReferralCode(generics.CreateAPIView):
+    serializer_class = users_serializers.CreateReferralCodeSerializer
+    queryset = users_models.ReferralCode.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
