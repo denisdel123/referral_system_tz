54332da9068e076581b996d6fd3def55f414180d
denisbelenko <denisdel123@>
2025-02-20 16:35:36 +0700

    добавил ссылку

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,8 @@ from . import views as users_views
 
 app_name = UsersConfig.name
 urlpatterns = [
-    path('registration', users_views.RegistrationUsers.as_view(), name='registration'),
+    path('registration/', users_views.RegistrationUsers.as_view(), name='registration'),
+    path('create-referral', users_views.CreateReferralCode.as_view(), name='create_referral'),
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
